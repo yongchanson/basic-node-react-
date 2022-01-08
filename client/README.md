bf9c5f9d4444eeec10140a2990bec873ff23475e
yongchanson <syc9430@gmail.com>
2022-01-08 22:45:32 +0900

    redux

--- client/README.md
+++ client/README.md
@@ -3,4 +3,9 @@
 - npx : 레지스트리에 있는 리액트 앱을 찾아서 사용 - > 항상 최신버전 사용가능 / Disk Space낭비x (글로벌로 다운받지 않음)
 - 이미지 등을 src에 담아야 webpack이 모아준다.
 - se7 설치 후 rfce : 기본 fuuction form 자동완성
--
+- poxy server : 인터넷 사용제어 / 캐쉬이용(빠른인터넷), 제한된사이트 접근가능 / 높은 보안(IP을 임의로 바꿔서 제공)
+- CSS FrameWork(React JS) : Material UI, React Boostrap, Semantic UI, Ant Design, Materialize
+- Props : 컴포넌트 간 주고받을때 사용 / 부모->자식 컴포넌트로만 보내기가능 / 자식 컴포넌트 내에서 props는 변할 수 없음(부모에서 다시 보내줘야함)
+- State : 컴포넌트 내에서 데이터교환or전달 시 사용 / State이 변함->re-render된다.
+- Redux : 상태관리 라이브러리(State을 관리하는 것)
+- Redux DevTools 크롬 확장프로그램 설치(리덕스 연결을 위한)
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import 'anth/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware()}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
