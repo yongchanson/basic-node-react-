09a01d695c2eb49324ebd76fab9fd05db7d1572e
yongchanson <syc9430@gmail.com>
2022-01-09 19:46:38 +0900

    login

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import {
+    LOGIN_USER,
+    REGISTER_USER,
+    AUTH_USER
+} from './types';
+export function loginUser(dataToSubmit) {
+
+    const request = axios.post('/api/users/login', dataToSubmit)
+        .then(response => response.data)
+
+    return {
+        type: LOGIN_USER,
+        payload: request
+    }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import 'anth/dist/antd.css';
+import 'antd/dist/antd.css';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
@@ -15,7 +15,10 @@ const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
 
 ReactDOM.render(
   <Provider
-    store={createStoreWithMiddleware()}
+  store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}
   >
     <App />
   </Provider>,
